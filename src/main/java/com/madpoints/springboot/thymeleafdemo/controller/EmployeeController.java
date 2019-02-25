c1378f23e6a4fd8f514541e00d1bc1631d5e9a04
John Furtado <jf8846@gmail.com>
2019-02-24 20:50:45 -0500

    removed hardcoded employee data, auto wired employee service

--- src/main/java/com/madpoints/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/madpoints/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,39 +1,29 @@
 package com.madpoints.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.madpoints.springboot.thymeleafdemo.model.Employee;
+import com.madpoints.springboot.thymeleafdemo.entity.Employee;
+import com.madpoints.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	private List<Employee> theEmployees;
+	private EmployeeService employeeService;
 	
-	@PostConstruct
-	private void loadData() {
-		
-		Employee employee1 = new Employee(1, "Alex", "Ashe", "Aa@email");
-		Employee employee2 = new Employee(2, "Bill", "Burns", "Bb@email");
-		Employee employee3 = new Employee(3, "Claire", "Collie", "Cc@email");
-		
-		theEmployees = new ArrayList<>();
-		
-		theEmployees.add(employee1);
-		theEmployees.add(employee2);
-		theEmployees.add(employee3);
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 	
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
+		
+		List<Employee> theEmployees = employeeService.findAll();
 		
 		theModel.addAttribute("employees", theEmployees);
 		
