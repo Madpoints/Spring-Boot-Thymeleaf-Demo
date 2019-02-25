b19e1b6d6c78b14f26dc3e830f499e7965cf80b5
John Furtado <jf8846@gmail.com>
2019-02-25 15:18:22 -0500

    method to order employees by last name ascending

--- src/main/java/com/madpoints/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/madpoints/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package com.madpoints.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.madpoints.springboot.thymeleafdemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	// sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
